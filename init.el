733d3357e3a8d2b05086c963b1689d7e527c357a
Koki Nomura <kokinomura1@gmail.com>
2017-02-03 10:08:43 +0900

    Update init.el

--- init.el
+++ init.el
@@ -15,28 +15,6 @@
 (add-to-list 'package-archives  '("marmalade" . "http://marmalade-repo.org/packages/"))
 (package-initialize)
 
-;; Ruby
-(autoload 'ruby-mode "ruby-mode"
-  "Mode for editing ruby source files" t)
-(add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Guardfile$" . ruby-mode))
-
-(add-hook 'ruby-mode-hook
-          '(lambda ()
-            (setq tab-width 2)
-            (setq ruby-indent-level tab-width)
-            (setq ruby-deep-indent-paren-style nil)
-            (define-key ruby-mode-map [return] 'ruby-reindent-then-newline-and-indent)))
-
-(autoload 'ruby-block "ruby-block" "Mode for highlighting blocks" t)
-(require 'ruby-block)
-(ruby-block-mode t)
-(setq ruby-block-highlight-toggle 'overlay)
-;; Rails
-(require 'web-mode)
-
 ;;; 適用する拡張子
 (add-to-list 'auto-mode-alist '("\\.phtml$"     . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tpl\\.php$" . web-mode))
@@ -55,21 +33,3 @@
   (setq web-mode-java-offset   2)
   (setq web-mode-asp-offset    2))
 (add-hook 'web-mode-hook 'web-mode-hook)
-
-;; Color theme
-(require 'color-theme)
-(color-theme-initialize)
-(color-theme-molokai)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(web-mode-html-tag-face ((t (:foreground "green"))))
- '(font-lock-comment-face ((t (:foreground "color-179")))))
